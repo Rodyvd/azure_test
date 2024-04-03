1255bb31dbe4b91c9846cfa77d172fcac50a8859
Rodyvd <114142546+Rodyvd@users.noreply.github.com>
2024-04-03 15:41:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,34 @@
-import os
+from flask import Flask, render_template, request, make_response
+from authomatic.adapters import WerkzeugAdapter
+from authomatic import Authomatic
+import authomatic
+from config import CONFIG
 
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+authomatic = Authomatic(CONFIG, 'JeroenIsMijnKat', report_errors=False)
 
-app = Flask(__name__)
-
-
+app = Flask(__name__, template_folder='.')
 @app.route('/')
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+@app.route('/login/<provider_name>/', methods=['GET', 'POST'])
+def login(provider_name):
+    # We need response object for the WerkzeugAdapter.
+    response = make_response()
+    # Log the user in, pass it the adapter and the provider name.
+    result = authomatic.login(WerkzeugAdapter(request, response), provider_name)
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
+    # If there is no LoginResult object, the login procedure is still pending.
+    if result:
+        if result.user:
+            # We need to update the user to get more info.
+            result.user.update()
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+        # The rest happens inside the template.
+        return render_template('login.html', result=result)
+
+    # Don't forget to return the response.
+    return response
 
 
 if __name__ == '__main__':
